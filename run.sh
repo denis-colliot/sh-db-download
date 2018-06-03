244f0cff83c6bd2800be37e4a49fc8d092c8acf7
Denis Colliot <denis.colliot@gmail.com>
2018-06-03 14:20:57 +0200

    Script update + gitignore

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
-curl -s 'http://mapix-data.ixxi.net/infos/info.prod.json' | jq '.url'
+JSON=`curl -s 'http://mapix-data.ixxi.net/infos/info.prod.json'`
+
+VERSION=`echo $JSON | jq '.version'`
+URL=`echo $JSON | jq -r '.url'`
+
+echo "Version $VERSION @ $URL"
+
+# Downloading referential.
+wget -q -N --tries=3 $URL
+
+FILENAME=$(basename "$URL")
+if hash gunzip 2>/dev/null; then
+  gunzip "$FILENAME"
+elif hash gzip 2>/dev/null; then
+  gzip -d "$FILENAME"
+else
+  echo "Requires 'gunzip' or 'gzip' command"
+  exit 1
+fi
